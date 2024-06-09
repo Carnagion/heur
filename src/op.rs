e2a74c13e1b59f1a67733752616b92e4d6846675
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-06-09 16:19:50 +0200

    Add basic impls for `Operator`

--- src/op.rs
+++ src/op.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{convert::Infallible, error::Error};
 
 // NOTE: We don't bound `E: Eval<S, P>` for a couple of reasons:
 //       1. Some operators don't use the evaluation function.
@@ -22,4 +22,108 @@ pub trait Operator<S, P, E, In = ()> {
         eval: &mut E,
         input: In,
     ) -> Result<Self::Output, Self::Error>;
+}
+
+impl<T, S, P, E, In> Operator<S, P, E, In> for &mut T
+where
+    T: Operator<S, P, E, In> + ?Sized,
+{
+    type Output = T::Output;
+
+    type Error = T::Error;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        solution: &mut S,
+        problem: &P,
+        eval: &mut E,
+        input: In,
+    ) -> Result<Self::Output, Self::Error> {
+        T::apply(self, solution, problem, eval, input)
+    }
+}
+
+impl<T, S, P, E, In> Operator<S, P, E, In> for Box<T>
+where
+    T: Operator<S, P, E, In> + ?Sized,
+{
+    type Output = T::Output;
+
+    type Error = T::Error;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        solution: &mut S,
+        problem: &P,
+        eval: &mut E,
+        input: In,
+    ) -> Result<Self::Output, Self::Error> {
+        T::apply(self, solution, problem, eval, input)
+    }
+}
+
+#[cfg(feature = "either")]
+impl<L, R, S, P, E, In> Operator<S, P, E, In> for either::Either<L, R>
+where
+    L: Operator<S, P, E, In>,
+    R: Operator<S, P, E, In, Output = L::Output, Error = L::Error>,
+{
+    type Output = L::Output;
+
+    type Error = L::Error;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        solution: &mut S,
+        problem: &P,
+        eval: &mut E,
+        input: In,
+    ) -> Result<Self::Output, Self::Error> {
+        match self {
+            Self::Left(left) => left.apply(solution, problem, eval, input),
+            Self::Right(right) => right.apply(solution, problem, eval, input),
+        }
+    }
+}
+
+impl<S, P, E> Operator<S, P, E> for () {
+    type Output = ();
+
+    type Error = Infallible;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        _solution: &mut S,
+        _problem: &P,
+        _eval: &mut E,
+        _input: (),
+    ) -> Result<Self::Output, Self::Error> {
+        Ok(())
+    }
+}
+
+impl<T, S, P, E, In> Operator<S, P, E, In> for Option<T>
+where
+    T: Operator<S, P, E, In>,
+{
+    type Output = Option<T::Output>;
+
+    type Error = T::Error;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        solution: &mut S,
+        problem: &P,
+        eval: &mut E,
+        input: In,
+    ) -> Result<Self::Output, Self::Error> {
+        self.as_mut()
+            .map(|op| op.apply(solution, problem, eval, input))
+            .transpose()
+    }
 }
