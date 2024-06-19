d0b06a6046b790d9164080a9feb7ae299a321a38
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-06-19 08:58:35 +0200

    Add `population::for_each`

--- src/op/population.rs
+++ src/op/population.rs
@@ -1,5 +1,26 @@
+use crate::{
+    eval::Eval,
+    solution::{Individual, Population},
+};
+
+use super::Operator;
+
+mod for_each;
+pub use for_each::ForEach;
+
 pub mod select;
 
 pub mod combine;
 
 pub mod insert;
+
+#[inline]
+pub fn for_each<P, S, E, In, T>(op: T) -> ForEach<T>
+where
+    T: Operator<P, Individual<S::Individual>, E, In, Output = In>,
+    S: Population + ?Sized,
+    for<'a> &'a mut S: IntoIterator<Item = &'a mut S::Individual>,
+    E: Eval<P, S::Individual>,
+{
+    ForEach(op)
+}
---
+++ src/op/population/for_each.rs
@@ -0,0 +1,38 @@
+use crate::{
+    eval::Eval,
+    op::Operator,
+    solution::{Individual, Population},
+};
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[must_use]
+pub struct ForEach<T>(pub(super) T);
+
+impl<T, P, S, E, In> Operator<P, S, E, In> for ForEach<T>
+where
+    T: Operator<P, Individual<S::Individual>, E, In, Output = In>,
+    S: Population + ?Sized,
+    // NOTE: This bound is currently satisfied by most population types except for `im::Vector<T>`. This seems to be an
+    //       oversight on `im`'s part and can be easily fixed.
+    for<'a> &'a mut S: IntoIterator<Item = &'a mut S::Individual>,
+    E: Eval<P, S::Individual>,
+{
+    type Output = In;
+
+    type Error = T::Error;
+
+    #[inline]
+    fn apply(
+        &mut self,
+        problem: &P,
+        population: &mut S,
+        eval: &mut E,
+        mut input: In,
+    ) -> Result<Self::Output, Self::Error> {
+        for solution in population {
+            let solution = Individual::from_mut(solution);
+            input = self.0.apply(problem, solution, eval, input)?;
+        }
+        Ok(input)
+    }
+}
